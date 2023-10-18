4ce0bb669dc85a4cca0983101e1517e9de0198ce
unknown <ntqcuong1993@gmail.com>
2023-10-18 11:19:32 +0700

    ok

--- src/Share/Chat/Chat.jsx
+++ src/Share/Chat/Chat.jsx
@@ -5,7 +5,7 @@ import ChatRoomsAPI from "../../API/ChatRoomsAPI";
 
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io("https://cellphone-be.onrender.com", {
   transports: ["websocket"],
 });
 
